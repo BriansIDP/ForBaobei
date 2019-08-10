e4634f71e84d0b8d311971d7ae6be0f4eba0b98c
BrianSun <gs534@cam.ac.uk>
2019-08-10 11:03:54 -0700

    change to python3

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-python clustering.py \
+python333 clustering.py \
     --number_of_users 100 \
     --jobs jobs.txt \
     --countries countries.txt \
     --embed used_word_embeddings.txt \
     --stddev 1.0 \
-    --threshold 0.95 \
+    --threshold 0.98 \
 
